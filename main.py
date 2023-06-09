7a3bf2c4575ac07e8652f4b8cf84746fd367dd43
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-09 17:47:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from chatbot import Chatbot
 from chatbot_gui import ChatbotGUI
 
 if __name__ == '__main__':
-    chatbot = Chatbot()
+    api_key = 'YOUR_API_KEY_HERE'  # Replace with your actual API key
+    chatbot = Chatbot(api_key)
     gui = ChatbotGUI(chatbot)
+
+    # Additional methods
+    def handle_order_intent(chatbot, input_text):
+        # Handle order-related queries
+        ...
+
+    def generate_default_response(chatbot, sentiment):
+        # Generate default response based on sentiment
+        ...
+
+    # Add the additional methods to the chatbot object
+    chatbot.handle_order_intent = handle_order_intent
+    chatbot.generate_default_response = generate_default_response
+
     gui.run()
